7c311f35393bbbe8539b88643067d78586ba3fe1
renatosiroma <renato.siroma@gmail.com>
2014-12-15 18:10:15 -0200

    teste

---
+++ facaafbfcec84.js
@@ -0,0 +1,3 @@
+var Clientes = new Array();
+Clientes[0] = '{"ct":"Q7QQLeAHhYURCRmxzexLTVxIqYRaiCBLLZrsGoset2M4us/atJ45PVDefnMOGZeI","iv":"f4b5e7016c40936c323a0723290d7292","s":"36d57534ebb7cc14"}';
+Clientes[1] = '{"ct":"g7K/419VuewQQsFgBkVAS0HcP2fX59Mjdqh0UoiFAKnHRD4I+xwX1DTn5I67SjtK","iv":"fde90f422afbb7de866625e761e31131","s":"81a31b5ae692f0bb"}';
---
+++ usuarios.js
@@ -0,0 +1,2 @@
+var usuarios = new Array();
+usuarios["1f830793a0c9a4afb56515f8ce311c05"] = 1;
